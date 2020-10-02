04447ba9ac59ef2d7df8a1fa047cc5f9d3baee44
ECHOPAIN <60045196+ECHOPAIN@users.noreply.github.com>
2020-10-02 15:01:09 +0200

    Update FragmentTwo.java

--- app/src/main/java/android/eservices/staticfragmenttabs/FragmentTwo.java
+++ app/src/main/java/android/eservices/staticfragmenttabs/FragmentTwo.java
@@ -1,20 +1,29 @@
 package android.eservices.staticfragmenttabs;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 //TODO : fix this fragment so it works :)
 //Once it's done, then create a second fragment with the other layout
-public class FragmentTwo extends Fragment {
+public class FragmentTwo extends Fragment{
+
+
+    private ViewPager mViewPager;
+    private Button button;
 
     public static final String TAB_NAME = "ADD TO COUNTER";
 
     public FragmentTwo() {
-        //Constructeur forcement vide (pas d'argument pas de comportement)
+        //Constructeur forcement vide (pas d'argument pas de comportement
     }
 
     public static FragmentTwo newInstance() {
@@ -25,7 +34,18 @@ public class FragmentTwo extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_two, container, false);
+        View view = inflater.inflate(R.layout.fragment_two, container, false);
+
+        button = (Button) view.findViewById(R.id.button_decrement);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText(getActivity(), "something to do", Toast.LENGTH_SHORT).show();
+                ((MainActivity)getActivity()).setCurrentCounter(((MainActivity)getActivity()).getCurrentCounter()-1);
+                ((MainActivity)getActivity()).updateCurrentCounterDisplay();
+            }
+        });
+        return view;
     }
 
     @Override
@@ -38,6 +58,14 @@ public class FragmentTwo extends Fragment {
         super.onStart();
     }
 
+
+
+    public void setViewPager(int fragmentNumber){
+        mViewPager.setCurrentItem(fragmentNumber);
+    }
+
     //TODO add listener to button and transmit the information to parent Activity
     //TODO read the Android doc, as suggested, to do it the right way
+
+
 }
